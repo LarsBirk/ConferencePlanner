050a99b0d8310b86484330d39a0269be6b3d9777
Lars <Lars@LAPTOP-FPV93TH9>
2022-08-09 08:37:50 +0200

    Add project files.

---
+++ GraphQL/AddSpeakerInput.cs
@@ -0,0 +1,7 @@
+﻿namespace ConferencePlanner.GraphQL
+{
+    public record AddSpeakerInput(
+        string Name,
+        string? Bio,
+        string? WebSite);
+}
---
+++ GraphQL/Query.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ConferencePlanner.GraphQL.Data;
+using HotChocolate;
+
+
+namespace ConferencePlanner.GraphQL
+{
+    public class Query
+    {
+        [UseApplicationDbContext]
+        public Task<List<Speaker>> GetSpeakers([ScopedService] ApplicationDbContext context) =>
+            context.Speakers.ToListAsync();
+    }
+}
